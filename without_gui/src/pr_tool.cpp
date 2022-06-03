606c5f9bf19521d53d6b7e5bd2219a1b6d6b2de7
Alexandre Amory <alexandre.amory@santannapisa.it>
2022-06-03 15:47:18 +0200

    fix interrupt number when there are multiple RRs

--- without_gui/src/pr_tool.cpp
+++ without_gui/src/pr_tool.cpp
@@ -268,9 +268,10 @@ void pr_tool::generate_fred_device_tree(flora *fl_ptr)
 		//write_dev_tree <<"\t\t\t\txlnx,s-axi-ctrl-bus-addr-width = <0x8>;" <<endl;
 		//write_dev_tree <<"\t\t\t\txlnx,s-axi-ctrl-bus-data-width = <0x20>;" <<endl;
 		write_dev_tree <<"\t\t\t\tinterrupt-parent = <&gic>;" <<endl;
-		write_dev_tree <<"\t\t\t\tinterrupts = <0x0 0x" << std::hex << first_interrupt + i <<" 0x4>;" <<endl;
+		write_dev_tree <<"\t\t\t\tinterrupts = <0 " << first_interrupt <<" 4>;" <<endl;
         write_dev_tree <<"\t\t\t}; " <<endl;
         write_dev_tree <<endl;
+        first_interrupt ++;
 
         // write_dev_tree <<"\t\t\tpr_decoupler_p"<<i<<"_s0@A00"<<std::hex << i+1<<"0000 { " <<endl;
         // write_dev_tree <<"\t\t\t\tcompatible = \"generic-uio\";" <<endl;
@@ -306,9 +307,10 @@ void pr_tool::generate_fred_device_tree(flora *fl_ptr)
         write_dev_tree <<"\t\t\tcompatible = \"generic-uio\";" <<endl;
         write_dev_tree <<"\t\t\treg = <0x43" <<std::hex << first_reg_addr + i <<"0000 0x10000>;" <<endl; 
         write_dev_tree <<"\t\t\tinterrupt-parent = <0x4>;" <<endl;
-        write_dev_tree <<"\t\t\tinterrupts = <0x0 0x"<< std::hex << first_interrupt + i <<" 0x4>; " <<endl;
+        write_dev_tree <<"\t\t\tinterrupts = <0 " << first_interrupt <<" 4>; " <<endl;
         write_dev_tree <<"\t\t}; " <<endl;
         write_dev_tree <<endl;
+        first_interrupt ++;
 
         write_dev_tree <<"\t\tpr_decoupler_p"<<i<<"_s0@43"<<std::hex << first_reg_addr + i + 1<<"0000 { " <<endl;
         write_dev_tree <<"\t\t\tcompatible = \"generic-uio\";" <<endl;
